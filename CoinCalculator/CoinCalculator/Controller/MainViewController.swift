c0dc9ac62a6e47f0a175e0ef468739163ace41f1
vkdlxh <tkdwnszoq@naver.com>
2018-01-31 15:49:23 +0900

    first init

---
+++ CoinCalculator/CoinCalculator/Controller/MainViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MainViewController.swift
+//  CoinCalculator
+//
+//  Created by jeonsangjun on 2018/01/31.
+//  Copyright © 2018年 archive-asia. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
