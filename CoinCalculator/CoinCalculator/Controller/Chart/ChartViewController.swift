aca2ff4a2e6df2ba993d784d57ec2b3e8d649c85
jaeeun <leejaeeun@archive-asia.co.jp>
2018-02-06 20:21:57 +0900

    Chart cocoapods追加、ひな形も。

--- CoinCalculator/CoinCalculator/Controller/Chart/ChartViewController.swift
+++ CoinCalculator/CoinCalculator/Controller/Chart/ChartViewController.swift
@@ -7,29 +7,217 @@
 //
 
 import UIKit
+import Charts
 
-class ChartViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+enum Option {
+    case toggleValues
+    case toggleIcons
+    case toggleHighlight
+    case animateX
+    case animateY
+    case animateXY
+    case saveToGallery
+    case togglePinchZoom
+    case toggleAutoScaleMinMax
+    case toggleData
+    case toggleBarBorders
+    // CandleChart
+    case toggleShadowColorSameAsCandle
+    // CombinedChart
+    case toggleLineValues
+    case toggleBarValues
+    case removeDataSet
+    // CubicLineSampleFillFormatter
+    case toggleFilled
+    case toggleCircles
+    case toggleCubic
+    case toggleHorizontalCubic
+    case toggleStepped
+    // HalfPieChartController
+    case toggleXValues
+    case togglePercent
+    case toggleHole
+    case spin
+    case drawCenter
+    // RadarChart
+    case toggleXLabels
+    case toggleYLabels
+    case toggleRotate
+    case toggleHighlightCircle
+    
+    var label: String {
+        switch self {
+        case .toggleValues: return "Toggle Y-Values"
+        case .toggleIcons: return "Toggle Icons"
+        case .toggleHighlight: return "Toggle Highlight"
+        case .animateX: return "Animate X"
+        case .animateY: return "Animate Y"
+        case .animateXY: return "Animate XY"
+        case .saveToGallery: return "Save to Camera Roll"
+        case .togglePinchZoom: return "Toggle PinchZoom"
+        case .toggleAutoScaleMinMax: return "Toggle auto scale min/max"
+        case .toggleData: return "Toggle Data"
+        case .toggleBarBorders: return "Toggle Bar Borders"
+        // CandleChart
+        case .toggleShadowColorSameAsCandle: return "Toggle shadow same color"
+        // CombinedChart
+        case .toggleLineValues: return "Toggle Line Values"
+        case .toggleBarValues: return "Toggle Bar Values"
+        case .removeDataSet: return "Remove Random Set"
+        // CubicLineSampleFillFormatter
+        case .toggleFilled: return "Toggle Filled"
+        case .toggleCircles: return "Toggle Circles"
+        case .toggleCubic: return "Toggle Cubic"
+        case .toggleHorizontalCubic: return "Toggle Horizontal Cubic"
+        case .toggleStepped: return "Toggle Stepped"
+        // HalfPieChartController
+        case .toggleXValues: return "Toggle X-Values"
+        case .togglePercent: return "Toggle Percent"
+        case .toggleHole: return "Toggle Hole"
+        case .spin: return "Spin"
+        case .drawCenter: return "Draw CenterText"
+        // RadarChart
+        case .toggleXLabels: return "Toggle X-Labels"
+        case .toggleYLabels: return "Toggle Y-Labels"
+        case .toggleRotate: return "Toggle Rotate"
+        case .toggleHighlightCircle: return "Toggle highlight circle"
+        }
     }
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+class ChartViewController: UIViewController, ChartViewDelegate {
+    
+    @IBOutlet var chartView: CandleStickChartView!
+    
+    var options: [Option]!
+    
+    var shouldHideData: Bool = false
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.initialize()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        self.initialize()
     }
-    */
-
+    
+    private func initialize() {
+        self.edgesForExtendedLayout = []
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        self.title = "BTC_JPY"
+        self.options = [.toggleValues,
+                        .toggleIcons,
+                        .toggleHighlight,
+                        .animateX,
+                        .animateY,
+                        .animateXY,
+                        .saveToGallery,
+                        .togglePinchZoom,
+                        .toggleAutoScaleMinMax,
+                        .toggleShadowColorSameAsCandle,
+                        .toggleData]
+        
+        chartView.delegate = self
+        
+        chartView.chartDescription?.enabled = false
+        
+        chartView.dragEnabled = false
+        chartView.setScaleEnabled(true)
+        chartView.maxVisibleCount = 200
+        chartView.pinchZoomEnabled = true
+        
+        chartView.legend.horizontalAlignment = .right
+        chartView.legend.verticalAlignment = .top
+        chartView.legend.orientation = .vertical
+        chartView.legend.drawInside = false
+        chartView.legend.font = UIFont(name: "HelveticaNeue-Light", size: 10)!
+        
+        chartView.leftAxis.labelFont = UIFont(name: "HelveticaNeue-Light", size: 10)!
+        chartView.leftAxis.spaceTop = 0.3
+        chartView.leftAxis.spaceBottom = 0.3
+        chartView.leftAxis.axisMinimum = 0
+        
+        chartView.rightAxis.enabled = false
+        
+        chartView.xAxis.labelPosition = .bottom
+        chartView.xAxis.labelFont = UIFont(name: "HelveticaNeue-Light", size: 10)!
+        
+//        sliderX.value = 10
+//        sliderY.value = 50
+        updateChartData()
+        
+        
+            for set in chartView.data!.dataSets as! [CandleChartDataSet] {
+                set.shadowColorSameAsCandle = !set.shadowColorSameAsCandle
+            }
+            chartView.notifyDataSetChanged()
+        
+        
+    }
+    
+    func updateChartData() {
+        if self.shouldHideData {
+            chartView.data = nil
+            return
+        }
+        
+        self.setDataCount(Int(10), range: UInt32(50))
+    }
+    
+    func setDataCount(_ count: Int, range: UInt32) {
+        let yVals1 = (0..<count).map { (i) -> CandleChartDataEntry in
+            let mult = range + 1
+            let val = Double(arc4random_uniform(40) + mult)
+            let high = Double(arc4random_uniform(9) + 8)
+            let low = Double(arc4random_uniform(9) + 8)
+            let open = Double(arc4random_uniform(6) + 1)
+            let close = Double(arc4random_uniform(6) + 1)
+            let even = i % 2 == 0
+            
+            return CandleChartDataEntry(x: Double(i), shadowH: val + high, shadowL: val - low, open: even ? val + open : val - open, close: even ? val - close : val + close, icon: nil)
+        }
+        
+        let set1 = CandleChartDataSet(values: yVals1, label: "Data Set")
+        set1.axisDependency = .left
+        set1.setColor(UIColor(white: 80/255, alpha: 1))
+        set1.drawIconsEnabled = false
+        set1.shadowColor = .darkGray
+        set1.shadowWidth = 0.7
+        set1.decreasingColor = .red
+        set1.decreasingFilled = true
+        set1.increasingColor = UIColor(red: 122/255, green: 242/255, blue: 84/255, alpha: 1)
+        set1.increasingFilled = false
+        set1.neutralColor = .blue
+        
+        let data = CandleChartData(dataSet: set1)
+        chartView.data = data
+    }
+    
+//    override func optionTapped(_ option: Option) {
+//        if .toggleShadowColorSameAsCandle ~= option {
+//            for set in chartView.data!.dataSets as! [CandleChartDataSet] {
+//                set.shadowColorSameAsCandle = !set.shadowColorSameAsCandle
+//            }
+//            chartView.notifyDataSetChanged()
+//        } else {
+//            super.handleOption(option, forChartView: chartView)
+//        }
+//    }
+//
+//    // MARK: - Actions
+//    @IBAction func slidersValueChanged(_ sender: Any?) {
+//        sliderTextX.text = "\(Int(sliderX.value))"
+//        sliderTextY.text = "\(Int(sliderY.value))"
+//
+//        self.updateChartData()
+//    }
+    
 }
+
