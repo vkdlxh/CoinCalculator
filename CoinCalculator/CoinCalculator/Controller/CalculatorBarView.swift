b3835c57e5872b0f70d22aa509072e6dc3863c5d
vkdlxh <tkdwnszoq@naver.com>
2018-02-15 17:07:10 +0900

    switch exchange mode 実装

--- CoinCalculator/CoinCalculator/Controller/CalculatorBarView.swift
+++ CoinCalculator/CoinCalculator/Controller/CalculatorBarView.swift
@@ -12,42 +12,80 @@ class CalculatorBarView: UIView {
     
     var ticker: Ticker? {
         didSet {
-            guard let ltp = ticker?.ltp else {
-                return
+            if let ticker = ticker {
+                if let productCode = ticker.productCode {
+                    self.coinName = productCode.convertCodeToName()
+                }
+                self.ltp = ticker.ltp
             }
-            self.ltp = ltp
+            
         }
     }
+    var coinName = ""
     var ltp: Int = 0
+    var isExchangeOfYenForCoin = false
+    
 //    var isKeyboardShowingByTappingView = false
     
-    @IBOutlet weak var coinValueLabel: UILabel!
-    @IBOutlet weak var jpyValueTextField: UITextField!
+    @IBOutlet weak var currentExchangeModeLabel: UILabel!
+    @IBOutlet weak var exchangedResultLabel: UILabel!
+    @IBOutlet weak var inputValueTextField: UITextField!
+    
+    @IBAction func switchExchangeMode(_ sender: UIButton) {
+        if isExchangeOfYenForCoin { // BTC -> JPY
+            currentExchangeModeLabel.text = "\(coinName) → JPY"
+            exchangedResultLabel.text = "JPY"
+            isExchangeOfYenForCoin = false
+        } else {                    // JPY -> BTC
+            currentExchangeModeLabel.text = "JPY → \(coinName)"
+            exchangedResultLabel.text = "\(coinName)"
+            isExchangeOfYenForCoin = true
+        }
+    }
     
     @IBAction func exchangeAction(_ sender: UIButton) {
-        // TODO: Exchange JPY -> Coin
-        if let jpyString = jpyValueTextField.text {
-            if let jpyDouble = Double(jpyString) {
-                let ltpDouble = Double(ltp)
-                let oneEn = 1 / ltpDouble
-                let exchangedValue = jpyDouble * oneEn
-                let finalValue = String(format:"%.8f", exchangedValue)
-                coinValueLabel.text = finalValue
+        if isExchangeOfYenForCoin { // JPY -> BTC
+            if let inputValueString = inputValueTextField.text {
+                if let inputValueDouble = Double(inputValueString) {
+                    let ltpDouble = Double(ltp)
+                    let oneYen = 1 / ltpDouble
+                    let exchangedValue = inputValueDouble * oneYen
+                    let finalValue = String(format:"%.8f", exchangedValue)
+                    exchangedResultLabel.text = finalValue
+                }
+            }
+        } else {                    // BTC -> JPY
+            if let inputValueString = inputValueTextField.text {
+                if let inputValueDouble = Double(inputValueString) {
+                    let ltpDouble = Double(ltp)
+                    let exchangedValue = Int(inputValueDouble * ltpDouble)
+                    
+                    let numFormatter : NumberFormatter = NumberFormatter()
+                    numFormatter.numberStyle = .decimal
+                    let finalValue : String = numFormatter.string(from: NSNumber(value: exchangedValue))!
+    
+                    exchangedResultLabel.text = "\(finalValue) ￥"
+                    
+                }
             }
         }
-        
-        
-        
-        
-        
     }
 
-    /*
+    
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
         // Drawing code
+        if isExchangeOfYenForCoin { // BTC -> JPY
+            currentExchangeModeLabel.text = "\(coinName) → JPY"
+            exchangedResultLabel.text = "JPY"
+            isExchangeOfYenForCoin = false
+        } else {                    // JPY -> BTC
+            currentExchangeModeLabel.text = "JPY → \(coinName)"
+            exchangedResultLabel.text = "\(coinName)"
+            isExchangeOfYenForCoin = true
+        }
     }
-    */
+ 
 
 }
